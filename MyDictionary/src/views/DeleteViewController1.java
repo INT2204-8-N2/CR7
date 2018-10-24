069701ba86323c4e575a40167e657825589abfcf
hai nd <duchaiuet@gmail.com>
2018-10-24 19:16:31 +0700

    .
    .

---
+++ MyDictionary/src/views/DeleteViewController1.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package views;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import model.MySQLDatabase;
+
+public class DeleteViewController1 implements Initializable {
+  MySQLDatabase b;
+  
+  
+    @FXML
+    private Label label;
+    private Button btInsert;
+    @FXML
+    private TextField txtWord;
+    @FXML
+    private TextArea txtDefine;
+    @FXML
+    private Button btDelete;
+
+    public DeleteViewController1() throws SQLException {
+        this.b = MySQLDatabase.getInstance("jdbc:mysql://localhost:3306/SINHVIEN","root","12345678" );
+    }
+    
+   
+    @FXML
+  public void handleButtonAction(ActionEvent event) {}
+ 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        btDelete.setOnAction((ActionEvent event1) -> {
+            Alert a = new Alert(Alert.AlertType.CONFIRMATION);
+              a.setTitle("Message");
+          if(txtWord.getText().length()>0){
+          try {
+              
+            if(b.detetetWord(txtWord.getText()) == 1){
+                System.out.println("delete sucessfully");
+                
+                a.setHeaderText("Delete Suceessfully");          
+                a.show();
+            }
+            else{
+                System.out.println("delete failed");
+                
+                a.setHeaderText("Delete Failed");          
+                a.show();
+            }
+          } catch (SQLException ex) {
+              Logger.getLogger(DeleteViewController1.class.getName()).log(Level.SEVERE, null, ex);
+          }
+          }else{
+              System.out.println("loi");
+                a.setHeaderText("Warning");          
+                a.show();
+          }
+      });
+    }    
+    
+}
---
+++ MyDictionary/src/views/MainViewController.java
@@ -0,0 +1,242 @@
+package views;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.Observable;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javazoom.jl.decoder.JavaLayerException;
+import model.Audio;
+import model.MySQLDatabase;
+import model.UrlGoogle;
+
+public class MainViewController implements Initializable{
+    Audio a = Audio.getInstance();
+   MySQLDatabase b;
+  //MyThread speakThread = new MyThread();
+  //MyThreadMySQL mySQL = new MyThreadMySQL();
+    @FXML
+    private TextField txtSearch  ; 
+    @FXML
+    private Button btSearch;
+    @FXML
+    private Button btInsert;
+    @FXML
+    private Button btUpdate;
+    @FXML
+    private ListView<String> listView;
+    @FXML
+    private Button btDelete;
+    @FXML
+    private Button btspeakers;
+    @FXML
+    private TextArea txtDefine;
+    @FXML
+    private Button bttran;
+
+    public MainViewController() throws SQLException {
+        this.b = MySQLDatabase.getInstance("jdbc:mysql://localhost:3306/SINHVIEN","root","12345678" );
+    }
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        
+    }
+      
+      public void initialize(URL url, ResourceBundle rb) {
+        System.out.println("views.MainViewController.handleButtonAction()");
+        txtSearch.addEventHandler(KeyEvent.KEY_RELEASED,new EventHandler<KeyEvent>(){
+            @Override
+            public void handle(KeyEvent event) {
+                System.out.println(".handle()");
+                try {
+                    if(txtSearch.getText().length()>0)
+                    listView.setItems( b.liketWord(txtSearch.getText()));
+                } catch (SQLException ex) {
+                    Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+        });
+        txtSearch.addEventHandler(KeyEvent.KEY_RELEASED,new EventHandler<KeyEvent>(){
+            @Override
+            public void handle(KeyEvent event) {
+                System.out.println(".handle()");
+                try {
+                    if(txtSearch.getText().length()>0)
+                    listView.setItems( b.liketWord(txtSearch.getText()));
+                    //listView.refresh();                  
+                } catch (SQLException ex) {
+                    Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+        });
+         
+        
+        
+        
+       bttran.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Alert a = new Alert(Alert.AlertType.CONFIRMATION);
+                a.setTitle("Message");
+                try {
+                    if(txtSearch.getText().length() > 0)
+                    {
+                        txtDefine.setText(UrlGoogle.tranlateText(txtSearch.getText()));
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    a.setHeaderText("EROR");          
+                    a.show();
+                }
+            }
+        });
+        
+        
+       
+       btSearch.setOnMouseClicked(new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent event) {
+                try {
+                    
+                    if(txtSearch.getText().length()>0){
+                    txtDefine.setText(b.selectWord(txtSearch.getText()));
+                    listView.focusTraversableProperty().unbind();
+
+                   if(txtDefine.getText().length()==0) 
+                   {
+                       Alert a = new Alert(Alert.AlertType.CONFIRMATION);
+                        a.setTitle("Message");
+                        a.setHeaderText("Warning");
+                        a.show();
+                   }
+                    }
+                } catch (SQLException ex) {
+                    Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+       });
+       btUpdate.setOnMouseClicked(new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent event) {
+               Parent root = null;
+        
+            try {
+                root = FXMLLoader.load(getClass().getResource("UpdateView.fxml"));
+            } catch (IOException ex) {
+                Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            Scene scene = new Scene(root);
+            Stage newWindow = new Stage();
+            newWindow.setTitle("Update Stage");
+            newWindow.setScene(scene);
+            newWindow.show();
+ 
+            }
+
+       });
+       btInsert.setOnMouseClicked(new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent event) {
+                 Parent root = null;
+        
+            try {
+                root = FXMLLoader.load(getClass().getResource("InsertView.fxml"));
+            } catch (IOException ex) {
+                Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            Scene scene = new Scene(root);
+            Stage newWindow = new Stage();
+            newWindow.setTitle("Insert Stage");
+            newWindow.setScene(scene);
+            newWindow.show();
+            }
+
+       });
+       btDelete.setOnMouseClicked(new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle(MouseEvent event) {
+                 Parent root = null;
+        
+            try {
+                root = FXMLLoader.load(getClass().getResource("DeleteView.fxml"));
+                
+            } catch (IOException ex) {
+                Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            Scene scene = new Scene(root);
+            Stage newWindow = new Stage();
+            newWindow.setTitle("Insert Stage");
+            newWindow.setScene(scene);
+            newWindow.show();
+            }
+
+       });
+       btspeakers.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event) {
+                System.out.println("Acess sucessfully");
+                try {
+                    if(txtSearch.getText().length()>0)
+                    a.play(a.Speech(txtSearch.getText(),"en"));
+                } catch (JavaLayerException ex) {
+                    Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ex) {
+                    Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
+                    
+                }
+            }
+           
+       });
+       
+       
+       
+   
+  
+   listView.getSelectionModel().selectedIndexProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
+       if(listView.getSelectionModel().getSelectedItem()!=null){
+           try {
+               txtDefine.setText(b.selectWord(listView.getSelectionModel().getSelectedItem()));
+           } catch (SQLException ex) {
+               Logger.getLogger(MainViewController.class.getName()).log(Level.SEVERE, null, ex);
+           }
+        
+       }
+       
+       });
+      }  
+    
+}
